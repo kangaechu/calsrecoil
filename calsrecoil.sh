c5b9200b48f1a8ffc50af2c4fb9372e5f66af06c
kangaechu <kangae2@gmail.com>
2025-05-18 20:55:36 +0900

    Add calsrecoil.sh script for event processing with usage instructions

---
+++ calsrecoil.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ set -euo pipefail
+
+# calsrecoil.sh
+# gcalcronから取得したパラメータを元に処理をする
+#
+# 使用方法
+# calsrecoil.sh <start_time> <end_time> <event_summary> <event_location> <event_description>
+#
+# パラメータ
+# start_time:        開始時刻 (2025-06-02T15:00:00+09:00")
+# end_time:          終了時刻 (2025-06-02T15:00:00+09:00")
+# event_summary:     タイトル
+# event_location:    場所
+# event_description: 説明
+
+exit 1
